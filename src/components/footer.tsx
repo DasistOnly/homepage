fcda6676b3e305300e8d35c3c301515fe9601eb1
Only <47639954+DasistOnly@users.noreply.github.com>
2024-07-11 10:53:00 +0200

    feat: seo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center w-full h-20 bg-background">
-      <p className="text-center text-md text-gray-500">
+      <p className="text-center text-md">
         &copy; 2024 Finn F
       </p>
     </footer>
