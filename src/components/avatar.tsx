da054971a525e5e3971e68b0f80eb84826531fb0
Only <47639954+DasistOnly@users.noreply.github.com>
2024-07-11 09:50:55 +0200

    feat: performance optimisation

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Avatar({ discordStatus }: { discordStatus: string }) {
   return (
-    <img
-      className="w-32 h-32 rounded-full mx-auto my-4 border-4"
+    <Image
+      className="rounded-full mx-auto my-4 border-4"
       src="https://github.com/dasistonly.png"
       alt="Avatar"
       style={{ borderColor: `#${discordStatus}`, padding: "2px" }}
+      width={128}
+      height={128}
     />
   );
 }
